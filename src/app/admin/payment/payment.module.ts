80bf29ee6f3901c111c3103304cdbbc3095a1b8d
Erick Eduardo Gomez Jimenez <erick@minimalist.mx>
2020-04-30 10:24:10 -0500

    Feature Add payment with Stripe

---
+++ src/app/admin/payment/payment.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaymentComponent } from './payment.component';
+import { ToasterModule } from 'angular2-toaster/angular2-toaster';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ToasterModule
+  ]
+})
+export class PaymentModule { }
